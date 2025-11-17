f4d32e8ce76724403c6a56a10c15707126f310c8
buicongthanh861 <buicongthanh861@gmail.com>
2025-11-17 18:13:19 +0700

    update file hotelOwner

---
+++ client/src/pages/hotelOwner/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../../components/Title'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Title align='left' font='outfit' title='Dashboard' subTitle=''/>
+    </div>
+  )
+}
+
+export default Dashboard
